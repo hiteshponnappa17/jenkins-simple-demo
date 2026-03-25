e36680aa76070cb33fb6d5e0331d9df750c35897
Hitesh Ponnappa <133213163+hiteshponnappa17@users.noreply.github.com>
2026-03-25 10:45:47 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,3 +1,14 @@
-#!/bin/bash
-echo "Hello from GitHub Project!"
-echo "CI/CD Pipeline Executed Successfully!"
+stage('Docker Build') {
+    steps {
+        sh "docker build -t hiteshponnappa77/myapp:${env.BUILD_NUMBER} ."
+    }
+}
+
+stage('Docker Push') {
+    steps {
+        sh "docker login -u hiteshponnappa77 -p ${DOCKER_HUB_PASSWORD}"
+        sh "docker push hiteshponnappa77/myapp:${env.BUILD_NUMBER}"
+        sh "docker tag hiteshponnappa77/myapp:${env.BUILD_NUMBER} hiteshponnappa77/myapp:latest"
+        sh "docker push hiteshponnappa77/myapp:latest"
+    }
+}
